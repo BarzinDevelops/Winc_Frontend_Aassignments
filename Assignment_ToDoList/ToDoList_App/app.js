6e90bdd16b7bd5d8d0d3982c9b94974a9994ad62
Barzin <barzin.development@gmail.com>
2022-09-15 14:06:30 +0200

    Write a piece of code to send the GET request and console.log the result so that you also see the empty array there (async/await!).

---
+++ Assignment_ToDoList/ToDoList_App/app.js
@@ -0,0 +1,18 @@
+const log = console.log;
+
+
+//Write a piece of code to send the GET request and console.log 
+// the result so that you also see the empty array there (think of async/await!).
+
+const dbURI = 'http://localhost:3000/';
+const testApi = async () => {
+    const resp = await fetch(dbURI, {
+        headers: {
+            "Content-Type": "application/json",
+        }
+    });
+    const respJSON = await resp.json();
+    log('resp=> ', respJSON);
+};
+
+testApi();
