29ec6e373d4e1a80fb7cebb8eb716289a1eb200b
Barzin <barzin.development@gmail.com>
2022-09-15 14:21:02 +0200

    Write code to be able to send new task with POST request. Test with previous getTask() if this is added to local db.

--- Assignment_ToDoList/ToDoList_App/app.js
+++ Assignment_ToDoList/ToDoList_App/app.js
@@ -4,6 +4,8 @@ const log = console.log;
 //Write a piece of code to send the GET request and console.log 
 // the result so that you also see the empty array there (think of async/await!).
 
+
+
 const dbURI = 'http://localhost:3000/';  // resource link
 const getTasks = async () => {
     const resp = await fetch(dbURI, {
@@ -15,6 +17,24 @@ const getTasks = async () => {
     log('resp=> ', respJSON);
 };
 
+// send a POST request to the URL, do this first with Postman and then also from JavaScript. 
+let task = {description: "make assignment ToDoList!", done: false};  // example task to use for POST request
+const setTasks = async (task)=>{
+    try {
+        fetch(dbURI, {
+            method: "POST",
+            body: JSON.stringify(task),
+            headers: {
+                "Content-Type": "application/json",
+            },
+          });
+    } catch (error) {
+        log(error);
+    }    
+};
+
+getTasks();
+setTasks(task);
 getTasks();
 
 
